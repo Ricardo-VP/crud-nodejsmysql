c3ffc1945511435d83c033d8e65e23c0b3b693da
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-27 17:46:18 -0500

    Agregando ruta para editar usuario

--- router.js
+++ router.js
@@ -24,4 +24,17 @@ router.get('/create', (req, res) => {
 const crud = require('./controllers/crud'); // llamando al controlador crud
 router.post('/save', crud.save); // post para guardar un nuevo usuario
 
+// Ruta para editar un usuario
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id; // obteniendo el id del usuario a editar
+    conexion.query('SELECT * FROM users WHERE id=?', [id], (err, result) => {
+        if (err){
+            throw err;
+        }else{
+            // renderizando la pagina edit con el resultado de la consulta
+            res.render('edit', {user: result[0]}) 
+        }       
+    })
+});
+
 module.exports = router; // exportando el router
