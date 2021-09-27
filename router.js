99691e85980673e9a088f7c22647cdbcffd0e903
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-27 17:21:43 -0500

    Agregando la ruta para crear un user

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router(); // llamando al router de express
 
 const conexion = require('./database/db'); // llamando a la conexion a la base de datos
 
+// Ruta para obtener todos los usuarios
 router.get('/', (req, res) => { // get para contacto
     
     conexion.query('SELECT * FROM users', (err, result) => {
@@ -14,5 +15,10 @@ router.get('/', (req, res) => { // get para contacto
     });
 
 });
+
+// Ruta para crear un nuevo usuario
+router.get('/create', (req, res) => {
+    res.render('create'); // renderizando la pagina create
+})
  
 module.exports = router; // exportando el router
