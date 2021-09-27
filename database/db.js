d8431eeda2d5554135b68be536c65df147e66c2b
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-27 02:29:29 -0500

    Arreglando clave de la db

--- database/db.js
+++ database/db.js
@@ -1,15 +1,20 @@
 const mysql = require('mysql');
 
+const dotenv = require('dotenv'); // Load .env file
+dotenv.config();
+const dbpassword = process.env.DB_PASSWORD;
+
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: dbpassword,
     database: 'crud_nodejs_mysql'
 });
 
 conexion.connect((err) => {
     if (err) {
         console.log(err);
+        console.log(dbpassword)
     } else {
         console.log('Conexion establecida con exito');
     }
--- router.js
+++ router.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router(); // llamando al router de express
 
-router.get('/contacto', (req, res) => { // get para contacto
-    res.send('contacto');
+const conexion = require('./database/db'); // llamando a la conexion a la base de datos
+
+router.get('/', (req, res) => { // get para contacto
+    conexion.query('SELECT * FROM users', (err, result) => {
+        if (err){
+            throw err;
+        }else{
+            res.send(result);
+        }
+    });
 });
  
 module.exports = router; // exportando el router
