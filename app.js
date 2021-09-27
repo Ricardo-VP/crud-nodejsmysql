b97cac7f3d02690d538178d16ad6550f448c355b
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-27 02:03:14 -0500

    Configurando el view engine y agregando las vistas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express'); // import express
 const app = express(); // create express app
 
+app.set('view engine', 'ejs'); // set view engine to ejs
+
 app.listen(5000, () => { // listen for requests on port 5000
     console.log('Server started on port 5000'); 
 });
